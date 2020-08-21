e00a0f6ffd1c0e7ed7bf1f2ba10e86374d16beea
ssujinchoi <wcsjinn@naver.com>
2020-08-21 19:38:25 +0900

    백준10974: 모든순열
    백준10974: 모든순열 풀이

---
+++ baekjoon10974/Beakjoon10974.java
@@ -0,0 +1,39 @@
+﻿import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	public static ArrayList<Integer> perArr;
+	public static int[] perCheck;
+	
+	private static void permutation(int n, ArrayList<Integer> perArr, int[] perCheck) {
+        if(perArr.size() == n){
+            for(int i : perArr){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+            return;
+        }
+         
+        for(int i=0; i<n; i++){
+            if(perCheck[i] == 0){
+            	perArr.add(i+1);
+                perCheck[i] = 1;
+                permutation(n, perArr, perCheck);
+                perCheck[i] = 0;
+                perArr.remove(perArr.size()-1);
+            }
+        }
+         
+    }
+	
+	public static void main(String[] args) {
+		int n;
+		Scanner sc = new Scanner(System.in);
+	
+		n = sc.nextInt();
+
+		perArr = new ArrayList<Integer>();
+		perCheck = new int[n];
+		permutation(n, perArr, perCheck);
+	}
+}
