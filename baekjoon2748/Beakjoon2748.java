b7419f9ca9f8e2521a4dcd2e88becc6857d39c3a
ssujinchoi <wcsjinn@naver.com>
2020-08-26 16:57:31 +0900

    백준2748: 피보나치2
    백준2748: 피보나치2 풀이

---
+++ baekjoon2748/Beakjoon2748.java
@@ -0,0 +1,26 @@
+﻿import java.util.*;
+
+public class Main {
+	static long[] fi;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+	
+		System.out.println(fibonnaci(n));
+	}
+	
+	public static long fibonnaci(int n) {
+		fi = new long[n+1];
+		
+		fi[0] = 0;
+		fi[1] = 1;
+		 
+		if(n>=2) {
+			for(int i=2; i<=n; i++) {
+				fi[i] = fi[i-1] + fi[i-2];
+			}
+		}
+		return fi[n];
+	}
+}
